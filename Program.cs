96af863cf37a3beadb897741f530c1f3077adea9
Marcos_JS <144068143+Marcosrtb@users.noreply.github.com>
2024-06-10 22:46:37 -0300

    Add files via upload
    Este código trata se de um exercício básico na inicialização da linguagem de programação: C#

---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace Calculadora;
+
+class Program
+{
+    static void Main(string[] args){
+        Console.WriteLine("## Calculadora Simples ##");
+        Console.WriteLine("\nOpções");
+
+
+        int opcao = 0;
+
+        do{
+        Console.WriteLine("1. Somar");
+        Console.WriteLine("2. Subtrair");
+        Console.WriteLine("3. Multiplicar");
+        Console.WriteLine("4. Dividir");
+        Console.WriteLine("0. Sair");
+        Console.WriteLine("\nEscolha uma opção: ");
+        opcao = Convert.ToInt32(Console.ReadLine());
+
+        if (opcao == 0)
+        break;
+        
+        Console.WriteLine("\nPrimeiro Valor: ");
+        Double valor1 = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Segundo Valor: ");
+        Double valor2 = Convert.ToDouble(Console.ReadLine());
+
+        Double resposta = 0;
+
+        switch(opcao){
+            case 1:
+            resposta = valor1  + valor2;
+            break;
+
+            case 2:
+            resposta = valor1 - valor2;
+            break;
+
+            case 3:
+            resposta = valor1 * valor2;
+            break;
+
+            case 4:
+            resposta = valor1 / valor2;
+            break;
+
+        }
+
+        Console.WriteLine("Resultado: " + resposta);
+
+        }
+        while(opcao != 0);
+
+    }
+}
